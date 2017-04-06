289e36a1765e628ffe0a0b3e50f9edbb702c5a81
Gabriel Ranghetti <granghetti@gmail.com>
2017-04-06 19:34:06 -0300

    mais debug de geolocation

--- web/assets/js/geolocalization.js
+++ web/assets/js/geolocalization.js
@@ -23,6 +23,7 @@ function getPosition(userId) {
                     });
                 },
                 function (failure) {
+                    console.log(failure.code+': '+failure.message);
                 }
             );
         } else {
